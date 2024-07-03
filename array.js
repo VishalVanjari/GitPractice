491c133c617e2abe51b365a3e21d8d079106f90d
Vishal <vishalrathod210302@gmail.com>
2024-07-03 17:58:00 +0530

    array file added

---
+++ array.js
@@ -0,0 +1,20 @@
+//Using an array literal 
+
+const fruits = ["Apple","Pineapple","Banana","Mango"];
+console.log(fruits);
+console.log(fruits[1]);
+
+
+const cars = [];
+cars[0] = "TATA";
+cars[1] = "BMW";
+cars[2] = "Toyato";
+console.log(cars);
+
+
+//Using New Keyword
+
+const languages = new Array("C","Java","Python","Nodejs");
+
+let lang = languages[1];
+console.log(lang);
